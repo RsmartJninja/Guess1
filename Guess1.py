3e234c932b5635371b87aa561c4e17463035581d
Rinja Marts <rsmartjninja@gmail.com>
2016-10-05 21:41:15 +0200

    3rd commit

--- Guess1.py
+++ Guess1.py
@@ -2,7 +2,7 @@
 secret = 7
 guess = int(raw_input ("Guess Number:"))
 print guess
-if (guess==secret):
+if (guess==secret):  #source http://stackoverflow.com/questions/32667736/how-to-check-if-a-dictionary-value-is-equal-to-a-variables-value
     print "Congratulation!"
 else:
     print "Wrong! Try it again!"
