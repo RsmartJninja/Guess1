e776cb30de0fc87bdc7f9fae42123d28229fcb0c
Rinja Marts <rsmartjninja@gmail.com>
2016-10-09 21:25:52 +0200

    FizzBuzz(easyversion) and String Lower Case

---
+++ FizzBuzz.py
@@ -0,0 +1,47 @@
+
+number1 = '1'
+number2 = '1', '2'
+number3 = '1', '2', 'fizz'
+number4 = '1', '2', 'fizz', '4'
+number5 = '1', '2', 'fizz', '4', 'buzz', 'fizz'
+number6 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7'
+number7 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7'
+number8 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8'
+number9 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz'
+number10 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', '10'
+number11 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', '10', '11'
+number12 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', '10', '11', 'fizz'
+number13 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', '10', '11', 'fizz', '13'
+number14 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', '10', '11', 'fizz', '13', '14'
+number15 = '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', '10', '11', 'fizz', '13', '14', 'fizzbuzz'
+fizzbuzz = int(raw_input("Select a number between 1 and 15: "))
+if fizzbuzz == 5:
+    print number5
+if fizzbuzz == 1:
+    print number1
+if fizzbuzz == 2:
+    print number2
+if fizzbuzz == 3:
+    print number3
+if fizzbuzz == 4:
+    print number4
+if fizzbuzz == 6:
+    print number6
+if fizzbuzz == 7:
+    print number7
+if fizzbuzz == 8:
+    print number8
+if fizzbuzz == 9:
+    print number9
+if fizzbuzz == 10:
+    print number10
+if fizzbuzz == 11:
+    print number11
+if fizzbuzz == 12:
+    print number12
+if fizzbuzz == 13:
+    print number13
+if fizzbuzz == 14:
+    print number14
+if fizzbuzz == 15:
+    print number15
---
+++ String Lower Case.py
@@ -0,0 +1,2 @@
+input = raw_input("Write something by using upper and lower cases and I will write all in lower cases: " ).lower ()
+print input.lower()
